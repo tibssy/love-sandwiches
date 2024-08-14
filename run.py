1cbad5ed633c965a77fc85acefc829a1c08e9243
Norbert Tiborcz <norbert.tiborcz@gmail.com>
2024-08-14 02:03:24 +0000

    Create get_sales_data function

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+# Your code goes here.
+# You can delete these comments, but do not change the name of this file
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,8 +16,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
 
-print(data)
+    print('Please enter sales data from the last market.')
+    print('Data should be six numbers, separated by commas.')
+    print('Example: 10,20,30,40,50,60\n')
+
+    data_str = input('Enter your data here: ')
+    print(f'The data provided is {data_str}')
+
+
+get_sales_data()
